2ef6ab4ee9d8b8042f193feb2368454dc4652065
jcchc <jcchc@cesar.school>
2025-10-22 15:10:40 -0300

    chore: configuração do projeto com apps e URLs

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'filmes',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+# Media files (uploads de usuários)
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
